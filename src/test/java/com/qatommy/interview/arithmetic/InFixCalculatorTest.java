7f3c850748e4cae69578b44d67709850745f119e
tommy stear <tom.stear@bodybuilding.com>
2015-09-15 14:47:16 -0600

    Updating dependencies to avoid using deprecated libraries.

--- src/test/java/com/qatommy/interview/arithmetic/InFixCalculatorTest.java
+++ src/test/java/com/qatommy/interview/arithmetic/InFixCalculatorTest.java
@@ -1,35 +1,36 @@
 package com.qatommy.interview.arithmetic;
 
-import com.sun.tools.javac.util.Pair;
 import org.junit.Test;
 
+import java.util.AbstractMap;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Map;
 
 import static org.junit.Assert.assertEquals;
 
 public class InFixCalculatorTest {
     @Test
     public void testCompute_expression() throws Exception {
-        for (Pair<String, Integer> pair : testValues) {
-            String s = pair.fst;
+        for (Map.Entry<String, Integer> pair : testValues) {
+            String s = pair.getKey();
             int actual = InFixCalculator.evaluate(s);
-            int expected = pair.snd;
+            int expected = pair.getValue();
             assertEquals("Failed to evaluate the string " + s + " properly.", expected, actual);
         }
     }
 
-    public static final List<Pair<String, Integer>> testValues = Arrays.asList(
-            new Pair<>("2+2", 4),
-            new Pair<>("(2+2)", 4),
-            new Pair<>("(2+2)-4", 0),
-            new Pair<>("((2+2)-4)", 0),
-            new Pair<>("(1+2)-4", -1),
-            new Pair<>("(2+2)-(3-(6-5))-4", -2),
-            new Pair<>("(3-(6-5))", 2),
-            new Pair<>("(3-(6-5))-4", -2),
-            new Pair<>("4-(3-(6-5))-4", -2),
-            new Pair<>("(2+2)-(3-(6-5))-4", -2)
+    public static final List<Map.Entry<String, Integer>> testValues = Arrays.asList(
+            new AbstractMap.SimpleEntry<>("2+2", 4),
+            new AbstractMap.SimpleEntry<>("(2+2)", 4),
+            new AbstractMap.SimpleEntry<>("(2+2)-4", 0),
+            new AbstractMap.SimpleEntry<>("((2+2)-4)", 0),
+            new AbstractMap.SimpleEntry<>("(1+2)-4", -1),
+            new AbstractMap.SimpleEntry<>("(2+2)-(3-(6-5))-4", -2),
+            new AbstractMap.SimpleEntry<>("(3-(6-5))", 2),
+            new AbstractMap.SimpleEntry<>("(3-(6-5))-4", -2),
+            new AbstractMap.SimpleEntry<>("4-(3-(6-5))-4", -2),
+            new AbstractMap.SimpleEntry<>("(2+2)-(3-(6-5))-4", -2)
     );
 
 }
