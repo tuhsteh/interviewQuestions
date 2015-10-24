086dd1f7bbfbc73787551de70555f0dcb6e3a552
tom stear <tuhsteh@users.noreply.github.com>
2015-10-23 18:18:51 -0600

    Update JustAsserts.java

--- src/main/java/com/qatommy/InterviewQuestions/JustAsserts.java
+++ src/main/java/com/qatommy/InterviewQuestions/JustAsserts.java
@@ -1,5 +1,11 @@
 package com.qatommy.InterviewQuestions;
 
+/**
+ * I once saw a plain old java "assert" operator call fail to behave the way I expected.
+ * I later learned the reason is that the assert operator will only do a comparison to true,
+ * not modify the world using any function inside the assert call.  
+ * In short, assert operations have no side-effects.
+ */ 
 public class JustAsserts {
 	public static void main(String[] args) {
 		assert (System.currentTimeMillis() > 1);
