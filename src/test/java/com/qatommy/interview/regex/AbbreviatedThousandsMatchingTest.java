1a62ae776e2cb007b52fdfda310d091287ac24d2
tom.stear <tom.stear@onedatascan.com>
2024-02-20 17:54:36 -0600

    readability

--- src/test/java/com/qatommy/interview/regex/AbbreviatedThousandsMatchingTest.java
+++ src/test/java/com/qatommy/interview/regex/AbbreviatedThousandsMatchingTest.java
@@ -16,22 +16,81 @@ public class AbbreviatedThousandsMatchingTest {
 		int bound = 99;
 		Random r = new Random(Instant.now().getEpochSecond());
 		final String outFormat = "%.0f";
+
+		// INTEGER COEFFICIENT WITH MIXED CASE MULTIPLIER
+		logger.info("INTEGER COEFFICIENT WITH MIXED CASE MULTIPLIER");
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				r.nextInt(bound) + "k"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				r.nextInt(bound) + "K"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				r.nextInt(bound) + "m"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				r.nextInt(bound) + "M")));
+
+		// DOUBLE COEFFICIENT WITH MIXED CASE MULTIPLIER
+		logger.info("DOUBLE COEFFICIENT WITH MIXED CASE MULTIPLIER");
 		final String inFormat = "%.1f";
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(r.nextInt(bound) + "k")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(r.nextInt(bound) + "K")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(r.nextInt(bound) + "m")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(r.nextInt(bound) + "M")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, bound * r.nextDouble()) + "k")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, bound * r.nextDouble()) + "K")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, bound * r.nextDouble()) + "m")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, bound * r.nextDouble()) + "M")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(-1 * r.nextInt(bound) + "k")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(-1 * r.nextInt(bound) + "K")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(-1 * r.nextInt(bound) + "m")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(-1 * r.nextInt(bound) + "M")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, -1 * bound * r.nextDouble()) + "k")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, -1 * bound * r.nextDouble()) + "K")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, -1 * bound * r.nextDouble()) + "m")));
-		logger.info(String.format(outFormat, AbbreviatedThousandsMatching.readNumberFromString(String.format(inFormat, -1 * bound * r.nextDouble()) + "M")));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, bound * r.nextDouble()) + "k"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, bound * r.nextDouble()) + "K"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, bound * r.nextDouble()) + "m"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, bound * r.nextDouble()) + "M"
+			)));
+
+		//  NEGATIVE INTEGER COEFFICIENT
+		logger.info("NEGATIVE INTEGER COEFFICIENT");
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				-1 * r.nextInt(bound) + "k"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				-1 * r.nextInt(bound) + "K"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				-1 * r.nextInt(bound) + "m"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				-1 * r.nextInt(bound) + "M"
+			)));
+
+		//  NEGATIVE DOUBLE COEFFICIENT
+		logger.info("NEGATIVE DOUBLE COEFFICIENT");
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, -1 * bound * r.nextDouble()) + "k"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, -1 * bound * r.nextDouble()) + "K"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, -1 * bound * r.nextDouble()) + "m"
+			)));
+		logger.info(String.format(outFormat,
+			AbbreviatedThousandsMatching.readNumberFromString(
+				String.format(inFormat, -1 * bound * r.nextDouble()) + "M"
+			)));
 	}
 }
